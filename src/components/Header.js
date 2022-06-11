6a22ef9739992fa869b5a76a8475881d39c8cbb4
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-06-12 01:17:04 +0900

    0612 header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+// css
+
+// react-icon
+import { AiOutlineHome } from 'react-icons/ai'
+import { GrDocumentVerified } from 'react-icons/gr'
 
 function Header() {
   return (
-    <div id='Header'>
-        
+    <div id='header'>
+        <div id='header-logo'>
+            <h1>Portfolio</h1>
+        </div>
+        <ul id='header-nav'>
+            <li><AiOutlineHome/></li>
+            <li><GrDocumentVerified/></li>
+            <li><img src='img/profile.jpeg'/></li>
+        </ul>
     </div>
   )
 }
